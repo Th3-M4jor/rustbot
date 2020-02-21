1da3878e35ca0c1f0af7349880fd87aff72697ad
Th3_M4j0r <spartan364@gmail.com>
2020-02-20 20:29:56 -0800

    NCPs were moved to a separate doc, adjusted link accordingly

--- src/library/ncp_library.rs
+++ src/library/ncp_library.rs
@@ -17,7 +17,7 @@ use std::fmt::Formatter;
 
 use unicode_normalization::UnicodeNormalization;
 
-const NCP_URL: &'static str = "https://docs.google.com/feeds/download/documents/export/Export?id=1cPLJ2tAUebIVZU4k7SVnyABpR9jQd7jarzix7oVys9M&exportFormat=txt";
+const NCP_URL: &'static str = "https://docs.google.com/feeds/download/documents/export/Export?id=1VhZSnjvwSTMxKKfJvKcwqaJDqxD_dXarmAlAYRmlV2k&exportFormat=txt";
 
 #[derive(Serialize)]
 #[serde(rename_all(serialize = "PascalCase"))]
