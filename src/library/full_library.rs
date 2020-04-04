59437752e40abd975776cd33575df2d0d280f9b9
Th3_M4j0r <spartan364@gmail.com>
2020-04-03 21:52:25 -0700

    add collector for full library

--- src/library/full_library.rs
+++ src/library/full_library.rs
@@ -1,19 +1,18 @@
 use std::collections::HashMap;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::RwLock;
 
+
 use crate::library::{
-    search_lib_obj,
-    battlechip::BattleChip,
-    ncp_library::NCP,
-    virus_library::Virus,
-    Library, LibraryObject,
+    battlechip::BattleChip, ncp_library::NCP, search_lib_obj, virus_library::Virus, Library,
+    LibraryObject,
 };
 //use crate::library::ncp_library::NCP;
 //use crate::library::search_lib_obj;
 //use crate::library::virus_library::Virus;
 //use crate::library::{Library, LibraryObject};
-use serenity::{model::channel::Message, prelude::*};
+use serenity::{model::channel::Message, prelude::*, model::permissions::Permissions};
 use simple_error::SimpleError;
 use std::fmt::Formatter;
 
@@ -92,16 +91,96 @@ impl Library for FullLibrary {
     }
 }
 
+
+const NUMBERS: &[&str] = &["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
+
+
 pub(crate) async fn search_full_library(ctx: &Context, msg: &Message, args: &[&str]) {
     let to_search = args.join(" ");
     let data = ctx.data.read().await;
     let library_lock = data.get::<FullLibrary>().expect("Full library not found");
     let library = library_lock.read().await;
+
     //.expect("library was poisoned, panicking");
+    let res : Vec<&str>;
     match search_lib_obj(&to_search, &library) {
-        Ok(val) => say!(ctx, msg, val),
-        Err(val) => say!(ctx, msg, format!("Did you mean: {},", val.join(", "))),
+        Ok(val) => {
+            say!(ctx, msg, val);
+            return;
+        }
+        Err(val) => res = val,
+        //say!(ctx, msg, format!("Did you mean: {},", val.join(", ")))
     }
+    
+    let mut msg_to_await : Message;
+
+    match msg.channel_id.say(ctx, format!("Did you mean: {}", res.join(", "))).await {
+        Ok(val) => msg_to_await = val,
+        Err(why) => {
+            println!("Could not send message: {:?}", why);
+            return;
+        }
+    }
+
+    let channel = match ctx.cache.read().await.guild_channel(msg.channel_id) {
+        Some(channel) => channel,
+        None => return,
+    };
+
+    let current_user_id = ctx.cache.read().await.user.id;
+    let permissions =
+        channel.read().await.permissions_for_user(ctx, current_user_id).await.unwrap();
+
+        if !permissions.contains(Permissions::ADD_REACTIONS | Permissions::MANAGE_MESSAGES) {
+            return;
+        }
+
+    for num in 0..res.len() {
+        if let Err(why) = msg_to_await.react(ctx, NUMBERS[num]).await {
+            println!("Could not react to message: {:?}", why);
+            return;
+        }
+    }
+    let mut got_proper_rection = false;
+    while !got_proper_rection {
+        if let Some(reaction) = &msg_to_await.await_reaction(&ctx).timeout(Duration::from_secs(15)).author_id(msg.author.id).await {
+            let emoji = &reaction.as_inner_ref().emoji.as_data();
+            let emoji_str = emoji.as_str();
+            for num in 0..res.len() {
+                if NUMBERS[num] == emoji_str {
+                    let to_say = search_lib_obj(res[num], &library);
+                    match to_say {
+                        Err(_) => {
+                            say!(ctx, msg, "An error occurred, inform the owner");
+                            return;
+                        }
+                        Ok(val) => {
+                            if let Err(why) = msg_to_await.edit(ctx, |m| m.content(val)).await {
+                                println!("Could not edit message: {:?}", why);
+                            }
+                            #[cfg(debug_assertions)]
+                            println!("Got a correct reaction, edited message");
+                            got_proper_rection = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        } else {
+            #[cfg(debug_assertions)]
+            println!("reaction wait timed out");
+            break;
+        }
+        #[cfg(debug_assertions)]
+        println!("Did not get a correct reaction, waiting again");
+    }
+
+    
+    if let Err(why) = msg_to_await.delete_reactions(ctx).await {
+        println!("Could not delete reactions: {:?}", why);
+    }
+
+    
     //say!(ctx, msg, search_lib_obj(&to_search, library));
 }
 
